3611c3719b17617d53e8a5311c0dadfc72375fe9
Stephanie H. Nguyen <byronic.string@gmail.com>
2016-01-23 12:37:43 -0800

    Amended server.

--- server.py
+++ server.py
@@ -9,18 +9,14 @@ app = Flask(__name__)
 
 app.secret_key = "###"
 
-
+@app.route("/", methods=["GET"])
+def say_hello():
+    return """ hihi """
 
 ##############################################################################
 
 if __name__ == "__main__":
     app.debug = True
-
-    connect_to_db(app)
-
-    # only run this once (on reload if in debug, or normal load if not debug)
-    if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-        install_alerts_daemon()
 
     app.run()
 
